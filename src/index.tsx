a60886b9b145e45fe6e21b52eaa5dab94747da0d
programador51 <gskitcat@gmail.com>
2022-11-27 20:19:56 -0600

    feat(ui kit done): semantic release configurated

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./global.scss";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import { Button } from "./atoms/button";
+import { Badge } from "./atoms/badge";
+import { H1, H2, H3, H4, H5, H6 } from "./atoms/heading";
+import { InputFile } from "./atoms/files/single";
+import { Checkbox } from "./atoms/checkbox";
+import { Switch } from "./atoms/switch";
+import { Password } from "./atoms/password/indext";
+import { Select } from "./atoms/select";
+import { Range } from "./atoms/range";
+import { Radio } from "./atoms/radio";
+import { InputText } from "./atoms/inputText";
+import { Modal } from "./structure/modal";
+import useModal from "./customHooks/useModal";
+import { ToastProvider } from "./structure/GlobalToast";
+import { BlockQuote } from "./molecule/blockquote";
+import { Pagination } from "./molecule/pagination";
+import { Alert } from "./molecule/alerts";
 import "./global.scss";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export {
+  Button,
+  Badge,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  InputFile,
+  Checkbox,
+  Switch,
+  Password,
+  Select,
+  Range,
+  Radio,
+  BlockQuote,
+  InputText,
+  Modal,
+  Pagination,
+  useModal,
+  ToastProvider,
+  Alert,
+};
